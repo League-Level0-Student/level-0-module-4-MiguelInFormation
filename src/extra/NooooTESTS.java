e2ae55c21b670eb1528a548459343091e0547fb1
ilabstaff <ilabstaff@172.16.116.67>
2019-05-08 19:23:26 -0700

    No Test = Competed | PiAloud = Almost Finished | StringMethods2 = Completed

---
+++ src/extra/NooooTESTS.java
@@ -0,0 +1,60 @@
+package extra;
+
+import javax.swing.JOptionPane;
+
+public class NooooTESTS {
+	
+	public static double Look=Double.parseDouble(boo); {
+		String boo=JOptionPane.showInputDialog("ok students turn in your tests the lowest grade is f and the highest grade is b ");
+		int Look=Integer.parseInt(boo);
+		if(Look>=90) {
+		JOptionPane.showMessageDialog(null,"You got a a+!" );
+		}
+		if(Look<=89) {
+		JOptionPane.showMessageDialog(null,"You got a B" );
+		}
+		if(Look<=70){
+		JOptionPane.showMessageDialog(null,"You got a B- " );
+		}
+		if(Look<=60) {
+		JOptionPane.showMessageDialog(null,"You got a C+ " );
+		}
+		if(Look<=55) {	
+		JOptionPane.showMessageDialog(null,"You got a c." );
+		}
+		if(Look<=30) {
+		JOptionPane.showMessageDialog(null,"You got a d+, very shamed." );
+		}
+		if(Look<=30) {
+		JOptionPane.showMessageDialog(null,"You got a D! Even MORE shame, but still shame." );
+		}
+		if(Look<=10) {
+		JOptionPane.showMessageDialog(null,"You got a D-! Shame on you!!!!!!!!!!!!!!" );
+		}
+		if(Look==0) {
+			JOptionPane.showMessageDialog(null,"You got an f! IDIOT" );
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+
+	
+}
+
+
