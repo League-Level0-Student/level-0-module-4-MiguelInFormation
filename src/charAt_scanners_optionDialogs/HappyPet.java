1413d2d3abc1b4edf2fd6925295c69320aa74ef7
ilabstaff <ilabstaff@mxtechimac82459.local>
2019-05-22 19:23:55 -0700

    COMMIT AND PUSH

--- src/charAt_scanners_optionDialogs/HappyPet.java
+++ src/charAt_scanners_optionDialogs/HappyPet.java
@@ -5,26 +5,65 @@ public class HappyPet {
 	
 	// 2. Add the following variable to the next line: static int happinessLevel = 0;
 	// this will be used to store the happiness of your pet
-	
+	private static int happinessLevel = 0;
 	public static void main(String[] args) {
-		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
-
-		// 7. REPEAT steps 3 - 6 enough times to make your pet happy!
 		
-			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
-			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
-			//    Make sure to customize the title and question too.
-			int task = JOptionPane.showOptionDialog(null, "Question", "Title", 0, JOptionPane.INFORMATION_MESSAGE, null,
-					new String[] { "Button1", "Button2", "Button3" }, null);
+	
+		// 1. Ask the user what kind of pet they want to buy, and store their answer in a variable
+		String Pet = JOptionPane.showInputDialog("What kind of pet would you like to buy?");
 
-			// 5. Use user input to call the appropriate method created in step 4.
+		//int task = JOptionPane.showOptionDialog(null, "WHAT WOULD YOU LIKE TO DO TO MAKE YOUR PET HAPPIER?", "HAPPY PET HAPPY LIFE", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Feed it","Walk it","Scratch it"},null);
 
-			// 6. If you determine the happiness level is large enough, tell the
+
+		
+		 while(true) {
+				int task = JOptionPane.showOptionDialog(null, "WHAT WOULD YOU LIKE TO DO TO MAKE YOUR PET HAPPIER?", "HAPPY PET HAPPY LIFE", 0, JOptionPane.INFORMATION_MESSAGE, null, new String [] {"Feed it","Walk it","Scratch it","yes"},null);
+
+			 if( task == 0) {
+				  feed();
+			  }		  
+			  else if(task == 1) {
+				  walk_it();
+			  }		  
+			  else if(task == 2) {
+			      scratch();	  
+			  }
+			  else {
+				  System.out.println("that is not an option");
+			  }	
+			 
+			 System.out.println (happinessLevel);
+			 if(happinessLevel== 100) {
+			   JOptionPane.showMessageDialog(null, "you love your " + Pet );
+			   break;
+			 }
+			 
+		 }
+
+	}	// 6. If you determine the happiness level is large enough, tell the
 			//    user that he loves his pet and use break; to exit for loop.
 
-	}
+	
 
 	// 4. Create methods to handle each of your user selections.
 	//    Each method should create a pop-up with the pet's response (eg. cat might purr when pet), 
 	//    and INCREMENT the pet's happiness Level.
+
+
+public static void feed() {
+//	JOptionPane
+	
+	happinessLevel += 10;
+}
+public static void walk_it() {
+//	JOptionPane
+	
+	happinessLevel += 10;
+}
+public static void scratch () {
+//	JOptionPane
+	
+	happinessLevel += 10;
+}
+
 }
