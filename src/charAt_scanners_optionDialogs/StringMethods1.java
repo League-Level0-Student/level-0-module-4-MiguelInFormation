614d64f94102a737fe7d522670b609d054678608
root <root@5f9f7172aabf>
2020-09-03 02:27:53 +0000

    sadads

--- src/charAt_scanners_optionDialogs/StringMethods1.java
+++ src/charAt_scanners_optionDialogs/StringMethods1.java
@@ -1,4 +1,7 @@
 package charAt_scanners_optionDialogs;
+
+import javax.swing.JOptionPane;
+
 //    Copyright (c) The League of Amazing Programmers 2013-2017
 //    Level 0
 
@@ -7,16 +10,9 @@ package charAt_scanners_optionDialogs;
 public class StringMethods1 {
 	public static void main(String[] args) {
 		
-		// 1. Create a String variable and initialize it to whatever you want
-
-		
-		              
-		String fruit=" SUSTAINABILITY";
-		System.out.println(fruit.charAt(0));
-		System.out.println(fruit.length());
-		for (int i = 0; i<fruit.length(); i++) {
-			System.out.println(fruit.charAt(i));
-		}
+		// 1. Create a String variable and initialize it to whateve
+		String input = "Abc";
+		System.out.println(input.charAt(2));
 		
 		
 		// 2. Print the 3rd char of your String to the console.
@@ -24,17 +20,24 @@ public class StringMethods1 {
 		
 		// 3. Print the length of your String to the console.
 		//    HINT: .length()
-
+System.out.println(input.length());
 		// 4. Using a for loop, print one char at time to the console.
 		//    HINT: use .length() to determine how many loops
 
-		
+		for (int i = 0; i < input.length(); i++) {
+			System.out.println(input.charAt(i));
+			
+		}
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
 		//    Print the char's position to the console.
 		//    EXAMPLE: if your string is "abc" and you are searching 
 		//             for char 'b', then print "b is at index 1"
 		
+		for (int k = 0; i < input.length(); k++) {
+			JOptionPane.showMessageDialog(null,+k+"is at index" );
+			
+		}
 	}
 }
 
